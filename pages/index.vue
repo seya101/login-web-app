4f573aa7670f21a534b9d3b5be88a8add29c63be
Hosea Patricia Corpuz <Hosea.Corpuz@EntainGroup.com>
2022-11-17 16:27:32 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+   <div class="context">
+     <div class="sm:flex sm:flex-row mx-0 justify-center">
+       <div class="flex-col flex  self-center p-10 sm:max-w-5xl xl:max-w-2xl  z-10">
+         <div class="self-start hidden lg:flex flex-col  text-white">
+           <h1 class="mb-3 font-bold text-5xl text-left">Welcome Back </h1>
+           <p class="pr-3">Lorem ipsum is placeholder text commonly used in the graphic, print,
+             and publishing industries for previewing layouts and visual mockups</p>
+         </div>
+       </div>
+       <div class="flex justify-center self-center  z-10">
+         <div class="p-12 bg-white mx-auto rounded-2xl w-100 ">
+             <div class="mb-4">
+               <h3 class="font-semibold text-2xl text-gray-800">Sign In </h3>
+               <p class="text-gray-500">Please sign in to your account.</p>
+             </div>
+             <div class="space-y-5">
+                   <div class="space-y-2">
+                             <label class="text-sm font-medium text-gray-700 tracking-wide">Email</label>
+                   <input class=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-amber-400" type="text" placeholder="email@gmail.com">
+               </div>
+                           <div class="space-y-2">
+               <label class="mb-5 text-sm font-medium text-gray-700 tracking-wide">
+                 Password
+               </label>
+               <input class="w-full content-center text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-amber-400" type="password" placeholder="******">
+             </div>
+               <div class="flex items-center justify-between">
+               <div class="flex items-center">
+                 <input id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 bg-blue-500 focus:ring-blue-400 border-gray-300 rounded">
+                 <label for="remember_me" class="ml-2 block text-sm text-gray-800">
+                   Remember me
+                 </label>
+               </div>
+               <div class="text-sm">
+                 <a href="#" class="text-amber-400 hover:text-amber-500">
+                   Forgot your password?
+                 </a>
+               </div>
+             </div>
+             <div>
+               <button type="submit" class="w-full flex justify-center bg-amber-400  hover:bg-amber-500 text-gray-100 p-3  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500">
+                 Sign in
+               </button>
+             </div>
+             </div>
+             <div class="pt-5 text-center text-gray-400 text-xs">
+                <span>
+                   Create Account? <NuxtLink to="register" class="text-amber-600 cursor-pointer"> Click Here</NuxtLink>
+                </span>
+             </div>
+         </div>
+       </div>
+   </div>
+    </div>
+ 
+ 
+ <div class="area" >
+     <ul class="circles">
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+     </ul>
+    </div >
+</template>
+ 
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/register.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+    <h1 class="text-5xl">Register</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
